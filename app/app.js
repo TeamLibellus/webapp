491c1ba0a1e7d1468cd73f69b94bc3d89dc79fde
Felix Fabrega <felixtao@Felixs-MBP.lan>
2016-02-20 19:38:41 -0800

    Base

---
+++ app/app.js
@@ -0,0 +1,25 @@
+libellus = angular.module('libellus', ['ngRoute', 'ngMaterial', 'ngMessages']);
+
+libellus.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+    when('/', {
+      templateUrl: 'app/partials/home.html',
+      controller: 'homeController'
+    }).
+    otherwise({
+      redirectTo: '/'
+    });
+  }
+])
+.config(['$httpProvider', function($httpProvider) {
+  $httpProvider.defaults.useXDomain = true;
+  $httpProvider.defaults.headers['Content-Type'] = 'application/json';
+  $httpProvider.defaults.withCredentials = true;
+}])
+.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+    .primaryPalette('orange')
+    .accentPalette('blue')
+    .warnPalette('red');
+});
