b98b23fafef31dd60cdb426b45b1e05e53316323
Guillaume Munsch <guigui.munsch@gmail.com>
2016-02-20 23:40:00 -0800

    Levels checkboxes

--- app/controllers/homeCtrl.js
+++ app/controllers/homeCtrl.js
@@ -3,12 +3,27 @@ libellus.controller('homeController', ['$scope', '$http', '$mdSidenav', '$log', 
   $scope.days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
   $scope.hours = ["8:00am", "9:00am", "10:00am", "11:00am", "12:00pm", "1:00pm", "2:00pm",
   "3:00pm", "4:00pm", "5:00pm", "6:00pm", "7:00pm", "8:00pm", "9:00pm", "10:00pm"];
+  $scope.levels = [100, 200, 300, 400, 500, 600];
 
   $scope.terms = [];
   $scope.subjects = [];
   $scope.classes = [];
+  $scope.selectedLevels = [];
 
   $scope.selectedTerm;
+
+  $scope.toggleSelection = function toggleSelection(level) {
+      var i = $scope.selectedLevels.indexOf(level);
+      if (i > -1) {
+        $log.debug("Del");
+        $scope.selectedLevels.splice(i, 1);
+      }
+      else {
+        $log.debug("Add");
+        $scope.selectedLevels.push(level);
+      }
+      $log.debug($scope.selectedLevels);
+    };
 
 $scope.openNavbar = function () {
   $mdSidenav('navbar').open()
