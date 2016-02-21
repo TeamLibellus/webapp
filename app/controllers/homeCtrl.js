dcd0992c877941b6e6646de7bcd587b33d21f2bb
Guillaume Munsch <guigui.munsch@gmail.com>
2016-02-20 20:26:14 -0800

    Terms, Subjects, Classes

--- app/controllers/homeCtrl.js
+++ app/controllers/homeCtrl.js
@@ -1,5 +1,47 @@
-libellus.controller('homeController', ['$scope', function($scope) {
+libellus.controller('homeController', ['$scope', '$http', function($scope, $http) {
 
-$scope.coucou = "coucou";
+  $scope.terms = [];
+  $scope.subjects = [];
+  $scope.classes = [];
+
+$scope.getTerms = function() {
+  $http({
+    method: 'GET',
+    url: 'http://api.libell.us/terms'
+  }).then(function successCallback(response) {
+    $scope.terms = response.data;
+    console.log($scope.terms);
+  }, function errorCallback(response) {
+    console.log(response);
+  });
+}
+
+$scope.getSubjects = function(term) {
+  $http({
+    method: 'GET',
+    url: 'http://api.libell.us/subjects'
+  }).then(function successCallback(response) {
+    $scope.subjects = response.data;
+    console.log($scope.subjects);
+  }, function errorCallback(response) {
+    console.log(response);
+  });
+}
+
+$scope.getClasses = function(subjectId) {
+  $http({
+    method: 'GET',
+    url: 'http://api.libell.us/subjects/' + subjectId + '/classes'
+  }).then(function successCallback(response) {
+    $scope.classes = response.data;
+    console.log($scope.classes);
+  }, function errorCallback(response) {
+    console.log(response);
+  });
+}
+
+// $scope.getTerms();
+// $scope.getSubjects(1);
+// $scope.getClasses(1);
 
 }]);
