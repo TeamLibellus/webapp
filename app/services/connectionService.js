d4e0af610170b52a91da8b454ae333110a9378fd
Guillaume Munsch <guigui.munsch@gmail.com>
2016-03-12 19:09:31 -0800

    Authentification service

---
+++ app/services/connectionService.js
@@ -0,0 +1,61 @@
+libellus.factory('AuthenticationService', ['$http', '$cookieStore', '$rootScope', '$location',
+    function ($http, $cookieStore, $rootScope, $location) {
+
+      var service = {};
+
+      var baseUrl = "http://api.libell.us/";
+
+        service.Login = function (username, password, success, failure) {
+          /* HASH LE MDP */
+          $http.post(baseUrl + 'login', {email:username, hash:password}).
+            then(function(response) {
+              service.SetCredentials(username);
+              success(response.data);
+            }, function(response) {
+              failure(response.data);
+          });
+        };
+
+        service.Logout = function() {
+          user = service.GetUser()
+          $http.post(baseUrl + 'logout').
+            then(function(response) {
+              service.ClearCredentials()
+              $location.path('/');
+            }, function(response) {
+          });
+        };
+
+        service.isLog = function (success, failure) {
+            $http.get(baseUrl + 'checklogin').
+              then(function(response) {
+                success();
+              }, function(response) {
+                failure();
+              });
+        };
+
+        service.GetUser = function () {
+          var cook = $cookieStore.get("globals")
+          var user;
+          if (cook) {
+            return cook.currentUser.username;
+          }
+        }
+
+        service.SetCredentials = function (username) {
+            $rootScope.globals = {
+                currentUser: {
+                    username: username
+                }
+            };
+            $cookieStore.put('globals', $rootScope.globals);
+        };
+
+        service.ClearCredentials = function () {
+            $rootScope.globals = {};
+            $cookieStore.remove('globals');
+        };
+
+        return service;
+    }])
